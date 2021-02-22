4e979abb12f9cc2a2eeacc9baa8cb38d1bc4a9f5
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2021-02-23 00:57:04 +0530

    completed the first ViewController

---
+++ BMI-Calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Pratik Sasmal on 23/02/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+
+
+class SecondViewController: UIViewController {
+    
+}
