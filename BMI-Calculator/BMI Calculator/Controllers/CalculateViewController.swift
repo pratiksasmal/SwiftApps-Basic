74857112db636d190ba918c5f9de2fb9ac0d6315
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2021-02-25 11:31:21 +0530

    Adding finishing touches and removing extra comments

--- BMI-Calculator/BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/CalculateViewController.swift
@@ -20,7 +20,6 @@ class CalculateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
@@ -29,7 +28,7 @@ class CalculateViewController: UIViewController {
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        let weight = String(format: "%.0f" ,sender.value)//Int(sender.value)
+        let weight = String(format: "%.0f" ,sender.value)
         weightLabel.text = "\(weight)Kg"
         
     }
@@ -40,24 +39,19 @@ class CalculateViewController: UIViewController {
         
         calculatorBrain.calculateBMI(height: height, weight: weight)
 
-        
         self.performSegue(withIdentifier: "goToResult", sender: self)
-        //let secondVC = CalculateViewController()
-        //secondVC.bmiValue = String(format: "%.1f", bmi)
-        //self.present(, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController
             //"as" will perform downcasting.
+            
             destinationVC.bmiValue = calculatorBrain.getBMIValue()
             //this connects to 1
             destinationVC.advice = calculatorBrain.getAdvice()
             destinationVC.color = calculatorBrain.getColor()
         }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
     
 }
--- BMI-Calculator/BMI Calculator/Models/CalculatorBrain.swift
+++ BMI-Calculator/BMI Calculator/Models/CalculatorBrain.swift
@@ -18,9 +18,6 @@ struct CalculatorBrain {
         return bmiTo1DecimalPlace
     }
     func getAdvice() -> String {
-        //using nil coalescing operator
-        //let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
-        //chaining BMI optional
         return bmi?.advice ?? "No advice for you Sire"
     }
     func getColor() -> UIColor {
@@ -30,15 +27,12 @@ struct CalculatorBrain {
     mutating func calculateBMI(height: Float, weight: Float){
         let bmiValue = weight / (height * height)
         if bmiValue < 18.5 {
-            //print("underweight")
-            bmi = BMI(value: bmiValue, advice: "Eat more", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Increase your meal size", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
         }
         else if bmiValue >= 18.5 && bmiValue <= 24.9 {
-            print("normal")
             bmi = BMI(value: bmiValue, advice: "You are in shape", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
         }
         else {
-            print("over")
             bmi = BMI(value: bmiValue, advice: "Eat less cookies", color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
         }
     }
