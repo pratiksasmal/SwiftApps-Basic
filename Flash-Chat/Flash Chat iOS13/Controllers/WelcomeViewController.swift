c41283802024f60505b155d149d5e43aa010a81c
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2021-02-27 23:55:46 +0530

    adding animations and timer to welcome page

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,19 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+       let titleText = "⚡️FlashChat"
+        
+        for letter in titleText {
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1  * charIndex, repeats: false){ (timer) in self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+            
+        }
     }
-    
 
 }
