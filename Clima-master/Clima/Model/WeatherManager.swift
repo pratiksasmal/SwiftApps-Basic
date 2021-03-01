2898fca717b2cdb7e936f703c7d61f88d2c5727a
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2021-03-01 14:24:00 +0530

    adding weatherModel and moving the condition acc to MVC

--- Clima-master/Clima/Model/WeatherManager.swift
+++ Clima-master/Clima/Model/WeatherManager.swift
@@ -43,32 +43,18 @@ struct WeatherManager {
         let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             //print(decodedData.weather[0].id)
             let id = decodedData.weather[0].id
-            print(getConditionName(weatherId: id))
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
         }
         catch {
             print(error)
         }
     }
     
-    func getConditionName(weatherId : Int) -> String{
-        switch weatherId {
-        case 200...232:
-            return "cloud.bolt"
-        case 300...321:
-            return "cloud.drizzle"
-        case 500...531:
-            return "cloud.rain"
-        case 600...622:
-            return "cloud.snow"
-        case 701...781:
-            return "cloud.fog"
-        case 800:
-            return "sun.max"
-        case 801...804:
-            return "cloud.bolt"
-        default:
-            return "cloud"
-        }
-    }
 }
 
